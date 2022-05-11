4d185f1a0c5e98ddce081a8572132a1c1ecfb1df
Jessica Allen <101675276+jessicaall@users.noreply.github.com>
2022-05-12 09:39:32 +1200

    Add files via upload
    04_v1, is basics of payout calculator
    04_v2, added some probability components
    04_v3, equal chance of getting all prizes
    04_v4, game has advantage with probabilities of:
    p(AU) = 0.1 | p(Ag)=0.2
    p(Cu) = 0.3 | p(Pb) = 0.4
    04_v5, game has bigger advantage with probabilities of:
    p(AU) = 0.05 | p(Ag)=0.2
    p(Cu) = 0.34| p(Pb) = 0.35
    AA_compiled_game_v1, first version of compiled game

---
+++ 04_prize_generation_v1.py
@@ -0,0 +1,12 @@
+# Decided to use randint to randonly generate all four of the numbers 1, 2, 3, 4
+
+import random
+
+
+NUM_TRIALS = 10
+
+for item in range(0, NUM_TRIALS):
+
+    # randint finds numbers between endpoints, including the endpoints
+    prizze_num = randome.randint(1,4)
+    print(prize_num)
